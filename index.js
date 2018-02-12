4b0be75da7045a928ec76929bfd15c4ba3775413
Duane Pearson <dp.anarchyink@gmail.com>
2018-02-12 13:18:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+function logWhisper(string) {
+ console.log('hello')
+ return string.toLowerCase()
+}
+function sayhiToGrandma(srting) {
+  if (string = 'hello') {
+ console.log("I can't hear you")
+  } else {
+    console.log()
+  }
+ 
