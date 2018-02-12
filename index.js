73cdc74b62a65ebeaee71ee17fa4bea68aaa55c3
Duane Pearson <dp.anarchyink@gmail.com>
2018-02-12 14:01:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ return string.toLowerCase()
   if (string.toLowerCase()===string) {
  return("I can't hear you")
   }
-  return string.toUpperCase()
-  if (string.toUpperCase===string) {
-    return("YES INDEED!")
-  }
+ 
   } 
  
